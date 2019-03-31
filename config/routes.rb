f97b52d878ce48bb43336e9595cb9ff721824819
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2019-03-31 19:54:14 +0300

    Feature User can sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  root to: "questions#index"
+
   resources :questions, only: %i[index show new create] do
     resources :answers, only: %i[show new create]
   end
