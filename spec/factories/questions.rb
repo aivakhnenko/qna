8c161b40dce1a942955458dcf80ab494a2469db3
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2019-03-31 17:09:09 +0300

    Feature User can see list of all questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "Title#{n}"
+  end
+
   factory :question do
     title { "MyString" }
     body { "MyText" }
+
+    trait :list do
+      title
+    end
 
     trait :invalid do
       title { nil }
