9eee09f4af1cb8d19dccb9dc1ffb7abd2f8b01d0
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2019-04-01 00:48:39 +0300

    Feature User can delete his answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,14 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, except: :show
-  before_action :find_question, only: %i[new create]
+  before_action :find_question, only: %i[new create destroy]
+  before_action :find_answer, only: %i[show destroy]
 
   def new
     @answer = @question.answers.new
   end
 
   def create
-    @answer = @question.answers.new(answer_params)
+    @answer = @question.answers.new(answer_params.merge(user: current_user))
     if @answer.save
       redirect_to [@question, @answer], notice: 'Your answer successfully created.'
     else
@@ -15,8 +16,15 @@ class AnswersController < ApplicationController
     end
   end
 
-  def show
-    @answer = Answer.find(params[:id])
+  def show; end
+
+  def destroy
+    if @answer.user == current_user
+      @answer.destroy
+      redirect_to @question, notice: 'Your answer successfully deleted.'
+    else
+      redirect_to @question, error: 'Only author can delete this question'
+    end
   end
 
   private
@@ -27,5 +35,9 @@ class AnswersController < ApplicationController
 
   def find_question
     @question = Question.find(params[:question_id])
+  end
+
+  def find_answer
+    @answer = Answer.find(params[:id])
   end
 end
