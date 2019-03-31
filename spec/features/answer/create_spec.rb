1432df7bda2eec72ebf02558f6214c71b700c07c
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2019-03-31 22:40:47 +0300

    Feature User can post answer - only authenticated users

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -6,27 +6,36 @@ feature 'User can post answer', %q{
   I'd like to be able to post the answer
 } do
 
-  scenario 'User posts an answer' do
-    question = create(:question)
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
 
-    visit question_path(question)
-    click_on 'Post answer'
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
 
-    fill_in 'Body', with: 'answer answer answer'
-    click_on 'Post'
+      visit question_path(question)
+      click_on 'Post answer'
+    end
 
-    expect(page).to have_content 'Your answer successfully created.'
-    expect(page).to have_content 'answer answer answer'
+    scenario 'posts an answer' do
+      fill_in 'Body', with: 'answer answer answer'
+      click_on 'Post'
+
+      expect(page).to have_content 'Your answer successfully created.'
+      expect(page).to have_content 'answer answer answer'
+    end
+
+    scenario 'tries to post an answer with errors' do
+      click_on 'Post'
+
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
-  scenario 'User tries to post an answer with errors' do
-    question = create(:question)
-
+  scenario 'Unauthenticated user tries post an answer' do
     visit question_path(question)
     click_on 'Post answer'
 
-    click_on 'Post'
-
-    expect(page).to have_content "Body can't be blank"
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
