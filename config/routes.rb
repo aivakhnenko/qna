576e26c1c627d0b5a25e8243ebaf95fcdd462ba0
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2020-02-04 10:07:43 +0300

    Add api/v1/profiles/me

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,13 @@ Rails.application.routes.draw do
   resources :links, only: :destroy
   resources :rewards, only: :index
 
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
+
   mount ActionCable.server => '/cable'
 end
