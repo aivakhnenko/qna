fcb49ba05fc08a51e21860d6b300a1780c5aec78
Alexander Ivakhnenko <alexander.ivakhnenko@gmail.com>
2020-02-07 05:57:26 +0300

    Add question reputation

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,15 @@ module Qna
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.autoload_paths += [config.root.join('app')]
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_job.queue_adapter = :sidekiq
+
+    config.autoload_paths += [config.root.join('app')]
+
     config.generators do |g|
       g.test_framework :rspec,
                        view_specs: false,
